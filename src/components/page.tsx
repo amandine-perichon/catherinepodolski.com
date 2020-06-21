17ba63f3af04ec592dbe92920a848f4d8b07c490
Amandine Perichon <apericho@gmail.com>
2020-06-21 21:38:13 +1200

    Fix navbar opacity and overlay opacity on mobile

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -6,13 +6,13 @@ import Link from './link'
 const Page: React.FC = ({ children }) =>
   <div>
     <nav className={styles.navbar}>
-      <Flex alignItems="center" justifyContent="space-between">
+      <Flex direction={["column", "row", "row", "row"]} alignItems="center" justifyContent="space-between">
         <div />
         <Link className={styles.link} to="/">
           <Text
             fontFamily="Sacramento"
-            lineHeight={["2rem", "2.5rem", "2.5rem", "2.5rem"]}
-            fontSize={["2rem", "2.5rem", "2.5rem", "2.5rem"]}
+            lineHeight={["1.5rem", "2.5rem", "2.5rem", "2.5rem"]}
+            fontSize={["1.5rem", "2.5rem", "2.5rem", "2.5rem"]}
           >
             Catherine Podolski
           </Text>
