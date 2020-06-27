290e6ede21d010cd8d14c575922c468d0841508f
Amandine Perichon <apericho@gmail.com>
2020-06-28 10:19:35 +1200

    Reorder decorative gallery

--- src/gallery/decorative.tsx
+++ src/gallery/decorative.tsx
@@ -22,24 +22,24 @@ import cera1747456 from '../../public/images/decorative/cera1747456.jpg'
 import cera1797948 from '../../public/images/decorative/cera1797948.jpg'
 
 const images = [
-  { source: cera0011 },
-  { source: cera0030 },
-  { source: cera004 },
-  { source: cera0069 },
-  { source: cera0096 },
-  { source: cera0114 },
-  { source: cera0136 },
-  { source: cera0149 },
-  { source: cera0159 },
-  { source: cera0164 },
-  { source: cera0171 },
-  { source: cera0299 },
-  { source: cera030 },
   { source: cera1626249 },
   { source: cera1626250 },
   { source: cera1636361 },
   { source: cera1747456 },
   { source: cera1797948 },
+  { source: cera0159 },
+  { source: cera0164 },
+  { source: cera0171 },
+  { source: cera0299 },
+  { source: cera0149 },
+  { source: cera004 },
+  { source: cera0011 },
+  { source: cera0030 },
+  { source: cera0069 },
+  { source: cera0096 },
+  { source: cera0114 },
+  { source: cera0136 },
+  { source: cera030 }
 ];
 
 const DecorativeGalleryPage: React.FC = () => <Page>
